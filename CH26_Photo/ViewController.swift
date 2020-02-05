0e22a9e0873eda2d82d5ae8a3cb7a10221dd249e
mac <meteoricdust1701@hotmail.com>
2020-02-05 10:42:30 +0800

    Initial Commit

---
+++ CH26_Photo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CH26_Photo
+//
+//  Created by mac on 2020/2/5.
+//  Copyright © 2020 mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
